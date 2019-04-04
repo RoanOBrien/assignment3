b559a7552057b77241491c96dbdb71ed60bb2712
Roan O'Brien <17088445@student.hhs.nl>
2019-04-04 11:26:51 +0200

    woohoo

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Function that allows the form to be activated on Enter keypressed
 (function () {
     console.log("is it working?");
     var input = document.getElementById("cityForm");
@@ -10,6 +11,7 @@
     })
 }());
 
+// Function that displays the time
 function time() {
     var today = new Date();
     var hours = today.getHours();
@@ -27,6 +29,7 @@ function time() {
 
 time();
 
+// Function that adds an extra 0 if time number is lower than 10
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
@@ -65,6 +68,7 @@ function getWeatherAPIdata() {
         });
 }
 
+// Function initialises when Openweathermap.org returns an OK response
 function onAPISucces(response) {
     // get type of weather in string format
     var type = response.weather[0].description;
@@ -79,6 +83,7 @@ function onAPISucces(response) {
     var coordinates = response.coord;
     console.log(coordinates);
 
+    // Function that gets the elevation from the coordinates given by Openweathermap.org
     function elevation() {
 
         var url = "https://elevation-api.io/api/elevation?points=";
@@ -86,6 +91,7 @@ function onAPISucces(response) {
         var coordLon = response.coord.lon;
         var coordLatLon = "(" + coordLat + "," + coordLon + ")";
 
+        // Fetch request for the API, no API key needed for this version of the API
         var request = url + coordLatLon;
 
         fetch(request)
@@ -96,7 +102,6 @@ function onAPISucces(response) {
             .then(function (response2) {
                 console.log(response2);
                 onElevationSuccess(response2);
-
                 var terrainElevation = response2.elevations[0].elevation;
                 checkPic(terrainElevation);
             });
@@ -121,12 +126,12 @@ document.getElementById("getWeather").onclick = function () {
     getWeatherAPIdata();
 };
 
+// Function that checks the elevation and based on that, shows the terrain type. Terrain below 150 m elevation is below sea level in the future
 function checkPic(elevation) {
     var terrainType = document.getElementById("show-terrain");
     if (elevation < 150) {
         terrainType.src = "./images/sea.gif";
         document.getElementById("displayTerrain").innerHTML = "Current terrain type: Sea";
-//        warning();
     } else if (elevation >= 150 && elevation < 1000) {
         terrainType.src = "./images/forest.gif";
         document.getElementById("displayTerrain").innerHTML = "Current terrain type: Forest";
@@ -135,20 +140,3 @@ function checkPic(elevation) {
         document.getElementById("displayTerrain").innerHTML = "Current terrain type: Desert";
     }
 }
-
-//function warning() {
-//    (function () {
-//        var tl1 = new TimelineMax({
-//            repeat: -1
-//        });
-//
-//        tl1.to('#warning', 0.5, {
-//                ease: Power0.easeNone,
-//                opacity: 1
-//            })
-//            .to('#warning', 0.5, {
-//                ease: Power0.easeNone,
-//                opacity: 0
-//            })
-//    }());
-//}
